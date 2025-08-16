6fa4411af7b6722ae03778ccf1340cd39f8d7969
jhaneya <arboladorajhaneya@gmail.com>
2025-08-16 11:53:28 +0000

    FIRST JAVA

---
+++ Name.java
@@ -0,0 +1,12 @@
+public class Name {
+
+    String first = "Theodore";
+    String last = " Moose";
+    String full = first + last;
+    public static void main(String[] hello) {
+        Name name = new Name();
+        name.first = "Jhaneya";
+        name.last = " Arboladora";
+        System.out.println(name.full);
+    }
+}
